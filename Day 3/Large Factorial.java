b2cd9af71c124e6d9192b4962ca169804e184da8
Swasti <64317533+SwastiPatra@users.noreply.github.com>
2021-07-24 12:13:40 +0530

    Add files via upload

--- Day 3/Large Factorial.java
+++ Day 3/Large Factorial.java
@@ -1,1 +1,12 @@
+import java.math.*;
+public class Solution {
+    public String solve(int A) {
+        BigInteger fact = new BigInteger("1");
 
+    for(int i = A; i > 0; i--){
+        fact = fact.multiply(BigInteger.valueOf(i));
+    }
+    
+    return fact + "";
+    }
+}

f41257ab2a1ea524ceb1152233fda97df78b493e
Swasti <64317533+SwastiPatra@users.noreply.github.com>
2021-07-24 12:06:50 +0530

    Delete Easy/Arrays directory

--- Easy/Arrays/ARRAY_BUG.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-	public ArrayList<Integer> rotateArray(ArrayList<Integer> A, int B) {
-		ArrayList<Integer> ret = new ArrayList<Integer>();
-		for (int i = 0; i < A.size(); i++) {
-			ret.add(A.get((i + B)%A.size()));
-		}
-		return ret;
-	}
-}
